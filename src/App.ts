3fea57ff325de34b8854a8a745449cea7b4ea39b
John Ovalle <johnovalle@gmail.com>
2018-05-27 14:27:56 -0400

    get a canvas up

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,10 @@
 import Message from './Message';
+import Canvas from './Canvas';
 
 const message = new Message();
 message.show();
+
+const canvas = new Canvas();
+canvas.attach(document.body);
+
+
