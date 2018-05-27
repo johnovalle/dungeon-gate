4ff253b9bd49d92e2b9518796379168b1991a4ed
John Ovalle <johnovalle@gmail.com>
2018-05-27 18:08:57 -0400

    transition between scenes with redux

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,10 @@
 import Message from './Message';
 import Stage from './Stage';
 import Renderer from './Renderer';
+import store from './Store';
+
+import { CHANGE_SCENE } from './actions/actiontypes';
+import { changeScene } from './actions/sceneActions';
 
 const message = new Message();
 message.show();
@@ -9,7 +13,19 @@ const stage = new Stage();
 stage.attach(document.body);
 
 const renderer = new Renderer(stage.ctx, stage.canvas);
-renderer.draw();
-renderer.drawText('Alive', '#00ff00', 100, 300);
+// renderer.draw();
+// renderer.drawText('Alive', '#00ff00', 100, 300);
 
+store.dispatch(changeScene('booting'));
+store.subscribe(() => {
+  console.log('store changed', store.getState());
+});
+
+setTimeout(()=> {
+  store.dispatch(changeScene('loading'));
+}, 2000);
+
+setTimeout(()=> {
+  store.dispatch(changeScene('opening'));
+}, 4000);
 
--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,16 +1,31 @@
+import store from './Store';
+
 export default class Renderer {
+  private _currentScene: Scene;
 
   constructor(private _ctx: CanvasRenderingContext2D, private _canvas: HTMLCanvasElement) {
-    
+    this._currentScene = store.getState().scenes.current;
+    store.subscribe(() => {
+      // console.log('store changed', store.getState());
+      this._currentScene = store.getState().scenes.current;
+      console.log('render', this._currentScene);
+      this.draw();
+    });
   }
 
-  draw(): void {
+  public draw(): void {
+    if (this._currentScene) {
+      this._drawBackground();
+    }
+  }
+
+  private _drawBackground(): void {
     this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
-    this._ctx.fillStyle = '#ff0066';
+    this._ctx.fillStyle = this._currentScene.background;
     this._ctx.fillRect(0, 0, this._canvas.width, this._canvas.height);
   }
 
-  drawText(text: string, color: string, x: number, y: number): void {
+  private _drawText(text: string, color: string, x: number, y: number): void {
     this._ctx.fillStyle = color;
     this._ctx.font = '20px Helvetica';
     this._ctx.fillText(text, x, y);
